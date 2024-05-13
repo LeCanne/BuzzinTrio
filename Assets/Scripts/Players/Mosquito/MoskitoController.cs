233f46057d548bb4e6c878f4163935c4d21d6389
Melvin Le Canne <94301906+LeCanne@users.noreply.github.com>
2024-05-13 16:57:23 +0200

    GravityTest

---
+++ Assets/Scripts/Players/Mosquito/MoskitoController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+public class MoskitoController : MonoBehaviour
+{
+
+    private Rigidbody rbMoskito;
+
+
+    [Header("PhysicsChecks")]
+    public LayerMask maskToCheck;
+    public float RaycastGroundCheckLenght;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbMoskito = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PhysicsUpdater();
+    }
+
+    public bool Grounded()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, RaycastGroundCheckLenght, maskToCheck))
+        {
+            Debug.DrawLine(transform.position, hit.point, Color.red);
+            return true;
+
+        }
+        else
+        {
+
+            return false;
+        }
+
+    }
+
+    private void PhysicsUpdater()
+    {
+        if(Grounded() == false && rbMoskito.velocity.y < 0)
+        {
+            rbMoskito.AddForce(Physics.gravity, ForceMode.Acceleration);
+        }
+    }
+}
+
+   
