bb265cff5425815fe08bea9cef7bcc0f9ca9b92f
Melvin Le Canne <94301906+LeCanne@users.noreply.github.com>
2024-06-18 13:54:24 +0200

    FX clap

--- Assets/Scripts/Players/Mosquito/MoskitoGeneral.cs
+++ Assets/Scripts/Players/Mosquito/MoskitoGeneral.cs
@@ -22,6 +22,7 @@ public class MoskitoGeneral : MonoBehaviour
     private AudioSource audioc;
 
     public bool deadforgood;
+    public ParticleSystem muzzleDed;
 
 
     // Start is called before the first frame update
@@ -93,7 +94,7 @@ public class MoskitoGeneral : MonoBehaviour
     }
     public void Die()
     {
-        
+        muzzleDed.Play();
         _stuckController.Unstucked();
 
         
